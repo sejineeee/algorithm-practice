11858fce8e3ca7cea2d817d16aa7462e7eaa3d55
sejineeee <sejinee0918@gmail.com>
2023-07-23 15:57:09 +0900

    Add: 5.js file
    반복문 내 조건문을 활용하여 연속되는 문자가 없을 경우
    해당 문자 배열에 push 및 number 초기화
    else 구문에서 중첩 조건문을 이용
    해당 index와 index + 1 문자가 같을 경우 숫자 1씩 증가
    index와 index + 1이 같지 않을 경우 배열에 해당 문자와 수 추가

---
+++ Section3/5.js
@@ -0,0 +1,28 @@
+// 문자열 압축
+// 알파벳 대문자로 이루어진 문자열을 입력받음
+// 같은 문자가 연속으로 반복되는 경우 반복되는 문자 바로 오른쪽에 반복 횟수 표기
+// 이러한 방법으로 문자열을 압축하는 프로그램 작성
+// 단, 반복횟수가 1인 경우 생략
+
+function compressString(string) {
+  let result = [];
+  let number = 1;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] !== string[i - 1] && string[i] !== string[i + 1]) {
+      result.push(string[i]);
+      number = 1;
+    } else {
+      if (string[i] === string[i + 1]) {
+        number += 1;
+      } else {
+        result.push(string[i]);
+        result.push(number);
+      }
+    }
+  }
+  return result.join("");
+}
+
+const str = "KKHSSSSSSSE";
+
+console.log(compressString(str));
