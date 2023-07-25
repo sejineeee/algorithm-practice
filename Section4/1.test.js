5b4fe63d391ae9b55d578c5e27225bc0e6bb921c
sejineeee <sejinee0918@gmail.com>
2023-07-25 15:08:17 +0900

    test: 자릿수의 합
    자릿수의 합 중 가장 큰 수를 반환하는 함수 test
    함수에 전달되는 값이 없을 때 에러 메시지 반환 test

---
+++ Section4/1.test.js
@@ -0,0 +1,17 @@
+const plusOfDigit = require("./1");
+
+describe("function plusOfDigit", () => {
+  it("returns the number with the largest sum of its digit", () => {
+    const result = plusOfDigit(488, 587, 695, 785, 794, 776, 767, 758, 794);
+
+    expect(result).toBe(794);
+  });
+
+  it("no value is passed to function, it throws an error message", () => {
+    const errorMessage = "값을 전달해주세요";
+
+    expect(() => {
+      plusOfDigit();
+    }).toThrow(errorMessage);
+  });
+});
