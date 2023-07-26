3a93e2e560e32eddf03fbc978d56928779c1e0e1
sejineeee <sejinee0918@gmail.com>
2023-07-26 10:47:56 +0900

    Add: 2.js file
    map method를 이용하여 배열을 순회
    if, else if 문을 이용하여 조건을 검사
    if, else if 문을 이용하는 것보다 깔끔한 방법 고민

---
+++ Section4/2.js
@@ -0,0 +1,35 @@
+// 뒤집은 소수
+// N개의 자연수가 입력되면 각 자연수를 뒤집은 후
+// 뒤집은 수가 소수이면 그 소수를 출력하는 프로그램
+// 첫 자리부터의 연속된 0은 무시한다.
+
+function getPrimeNum(...numbers) {
+  let answer = [];
+  numbers.map((number) => {
+    let reverseNum = Number(number.toString().split("").reverse().join(""));
+
+    if (reverseNum === 1) {
+      return;
+    } else if (
+      reverseNum === 2 ||
+      reverseNum === 3 ||
+      reverseNum === 5 ||
+      reverseNum === 7
+    ) {
+      answer.push(reverseNum);
+    } else if (reverseNum % 2 === 0) {
+      return;
+    } else if (reverseNum % 3 === 0) {
+      return;
+    } else if (reverseNum % 5 === 0) {
+      return;
+    } else if (reverseNum % 7 === 0) {
+      return;
+    } else {
+      answer.push(reverseNum);
+    }
+  });
+  return answer;
+}
+
+getPrimeNum(32, 55, 62, 20, 250, 370, 200, 30, 100);
