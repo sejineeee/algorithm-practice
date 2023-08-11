24a31159aedfb0f4b9110a7d7066dd39771b7ef4
sejineeee <sejinee0918@gmail.com>
2023-08-11 11:48:29 +0900

    Add: 5.js file
    K번째 큰 수
    for문 3중첩을 이용
    중복을 허용하지 않는 Set을 이용
    정렬하기 위해 Set을 배열로 만든 다음 sort method 이용
    k번째로 큰 수를 출력하기 위해 slice method 이용

---
+++ Section4/5.js
@@ -0,0 +1,31 @@
+// K번째 큰 수
+// 1 ~ 100 사이의 자연수가 적힌 N장의 카드가 있음
+// 같은 수의 카드가 여러 장 있을 수 있음
+// 이 중 3장을 뽑아 각 카드에 적힌 수를 합한 값을 기록
+// 모든 경우를 기록하여 기록한 값 중 K번째로 큰 수를 출력하는 프로그램
+
+function getNthSumOfCard(numberOfCards, k, cards) {
+  let resultOfSum = new Set();
+  let result;
+  for (let i = 0; i < numberOfCards; i++) {
+    for (let j = 0; j < numberOfCards; j++) {
+      if (i === j) {
+        continue;
+      }
+      for (let m = 0; m < numberOfCards; m++) {
+        if (i === m || j === m) {
+          continue;
+        }
+        resultOfSum.add(cards[i] + cards[j] + cards[m]);
+      }
+    }
+  }
+
+  let sortedResult = [...resultOfSum].sort((a, b) => b - a);
+  result = sortedResult.slice(k - 1, k);
+  return result;
+}
+
+const cardList = [13, 15, 34, 23, 45, 65, 33, 11, 26, 42];
+
+console.log(getNthSumOfCard(10, 3, cardList));
