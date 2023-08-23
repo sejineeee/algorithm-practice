25eb7c017b2028dd729632c96f5a8f5b726ff273
sejineeee <sejinee0918@gmail.com>
2023-08-23 14:49:26 +0900

    Add: 4.js file
    연속부분수열
    two pointers algorithm 이용
    경우를 구하기 위해서 slice method 적용
    if 조건문을 이용하여 조건에 충족할 때 result 배열에 추가
    sum과 M이 동일한 경우에는 point1에 1을 더하여 start 지정
    경우의 수를 구하기 위해 length 속성 이용

---
+++ Section5/4.js
@@ -0,0 +1,46 @@
+// 연속 부분수열
+// N개의 수로 이루어진 수열이 주어짐
+// 연속부분수열의 합이 특정숫자 M이하가 되는 경우가 몇 번 있는지 구하는 프로그램
+// 만약 N = 5, M = 5이고 수열이 1 3 1 2 3 이라면 합이 5 이하가 되는 연속부분수열은
+// {1}, {3}, {1}, {2}, {3}, {1, 3}, {3, 1}, {1, 2}, {2, 3}, {1, 3, 1}로 총 10가지
+
+function getMOrLess(M, seq) {
+  const N = seq.length;
+  const result = [];
+
+  let sum = 0;
+  let point1 = 0;
+
+  for (let point2 = 0; point2 < N; point2++) {
+    sum += seq[point2];
+    if (seq[point2] <= M) {
+      result.push([seq[point2]]);
+    }
+
+    if (sum <= M) {
+      if (point1 === point2) {
+        continue;
+      }
+      result.push(seq.slice(point1, point2 + 1));
+    }
+
+    if (sum === M) {
+      result.push(seq.slice(point1 + 1, point2 + 1));
+    }
+
+    while (sum > M) {
+      sum -= seq[point1];
+      point1++;
+      if (sum <= M) {
+        result.push(seq.slice(point1, point2 + 1));
+      }
+    }
+  }
+
+  console.log(result);
+  return result.length;
+}
+
+const sequence = [1, 3, 1, 2, 3];
+
+console.log(getMOrLess(5, sequence));
