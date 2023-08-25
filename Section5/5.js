1faa5eb54ab6ffd11b6e17f25d184f66418907e7
sejineeee <sejinee0918@gmail.com>
2023-08-25 14:26:00 +0900

    Test: function module exports

--- Section5/5.js
+++ Section5/5.js
@@ -35,3 +35,5 @@ function getConsecutiveMaxSales(consecutiveDays, salesList) {
 const list = [12, 15, 11, 20, 25, 10, 20, 19, 13, 15];
 
 console.log(getConsecutiveMaxSales(3, list));
+
+module.exports = getConsecutiveMaxSales;
