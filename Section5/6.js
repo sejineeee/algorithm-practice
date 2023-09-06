d508ec4c4ba4dbca14a98d87a9d907b585e34d1b
sejineeee <sejinee0918@gmail.com>
2023-09-06 23:40:08 +0900

    Add: 6.js file
    for문을 이용하여 문자열 순회하여 객체에 문자를 key값으로 카운팅은 value
    만든 객체를 for in 문으로 순회하며 값을 비교

---
+++ Section5/6.js
@@ -0,0 +1,31 @@
+// 학급 회장(해쉬)
+// 학급 회장 후보 기호 A, B, C, D, E 등록
+// 어떤 기호가 학급 회장이 되었는지 출력하는 프로그램
+// 첫 줄에는 반 학생수가 주어지고, 두번째 줄에는 선생님이 발표한 순서 문자열
+// 반드시 한 명의 학급회장이 선출되도록 투표결과가 나왔다고 가정함
+
+function getVotingResult(N, s) {
+  let obj = {};
+  let num = null;
+  let result = null;
+
+  for (let i = 0; i < N; i++) {
+    if (obj[s[i]]) {
+      obj[s[i]]++;
+    } else {
+      obj[s[i]] = 1;
+    }
+  }
+
+  for (const candidate in obj) {
+    if (num < obj[candidate]) {
+      num = obj[candidate];
+      result = candidate;
+    }
+  }
+  return result;
+}
+
+let str = "BACBACCACCBDEDE";
+
+console.log(getVotingResult(15, str));
