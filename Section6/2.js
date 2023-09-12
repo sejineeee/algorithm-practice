4f3df2be5664c316b5596ed23d7e7fb18863ef9f
sejineeee <sejinee0918@gmail.com>
2023-09-12 14:40:46 +0900

    Add: 2.js file
    스택 자료구조는 배열을 이용하여 문제 풀이
    for of문을 이용하여 문자열 순회하며 조건문 검사
    배열의 길이가 0이 되고 pop 메서드가 반환한 값이 괄호가 아닌 조건에 문자열 push
    join 메서드를 이용하여 배열을 문자열로 변환하여 반환

---
+++ Section6/2.js
@@ -0,0 +1,26 @@
+// 괄호 문자 제거
+// 입력된 문자열에서 소괄호 () 사이에 존재하는 모든 문자를 제거하고
+// 남은 문자만 출력하는 프로그램
+
+function removeStr(s) {
+  const stack = [];
+  const resultString = [];
+  for (const str of s) {
+    let popValue = "";
+    if (str === "(") {
+      stack.push(str);
+    } else if (str === ")") {
+      popValue = stack.pop();
+    }
+
+    if (!stack.length && popValue !== "(") {
+      resultString.push(str);
+    }
+  }
+
+  return resultString.join("");
+}
+
+const str = "(A(BC)D)EF(G(H)(IJ)K)LM(N)";
+
+console.log(removeStr(str));
