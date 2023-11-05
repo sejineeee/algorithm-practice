195cc3a14b7df76e5fc6d4f2b0219cf911b8c6af
sejineeee <sejinee0918@gmail.com>
2023-11-05 23:38:22 +0900

    Add: 교육과정설계 알고리즘 문제 풀이

---
+++ Section6/7.js
@@ -0,0 +1,34 @@
+// 교육과정 설계
+// 1년 과정의 수업계획, 필수 과목 O, 순서 O, 이수 필수
+// 수업계획서상의 각 과목은 무조건 이수된다고 가정
+// 수업설계가 잘된 것 -> "YES", 잘못된 것 -> "NO" 출력
+
+function checkDesignedSubjects(required, designed) {
+  const requiredSubjects = required.split('');
+  const designedSubjects = designed.split('');
+
+  let result = '';
+
+  designedSubjects.forEach((subject) => {
+    if (requiredSubjects.includes(subject)) {
+      if (subject === requiredSubjects[0]) {
+        requiredSubjects.shift();
+      }
+    }
+
+    result = 'NO';
+
+    if (!requiredSubjects.length) {
+      result = 'YES';
+    }
+  });
+
+  return result;
+}
+
+const requiredSubjectsString = 'CBA';
+const designedSubjectsString = 'CBDAGE';
+
+console.log(
+  checkDesignedSubjects(requiredSubjectsString, designedSubjectsString)
+);
