df9565341ab2c21765c2567bd991d46381e10473
sejineeee <sejinee0918@gmail.com>
2023-11-05 23:59:17 +0900

    Test: 수업 설계의 길이 30자 이상인 경우 에러 반환 테스트 추가

--- Section6/7.test.js
+++ Section6/7.test.js
@@ -18,4 +18,13 @@ describe('function checkDesignedSubjects', () => {
 
     expect(result).toBe('NO');
   });
+
+  it('수업설계의 길이가 30 이상일 경우 30 이하 안내 문구를 반환한다', () => {
+    const requiredSubjects = 'CBA';
+    const designedSubjects = 'ABCDEFGHIKJLMNOPQRSTUVWXYZWERTW';
+
+    expect(() => {
+      checkDesignedSubjects(requiredSubjects, designedSubjects);
+    }).toThrowError('수업설계의 길이는 30자 이하여야 됩니다');
+  });
 });
