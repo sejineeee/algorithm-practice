69c607d44d38d6dc7df34bf313312786820d6b7b
sejineeee <sejinee0918@gmail.com>
2023-12-09 23:00:12 +0900

    Add: 팩토리얼 구하기

---
+++ Section8/11.js
@@ -0,0 +1,20 @@
+// 팩토리얼
+// 자연수 N을 입력하면 N값을 구하세요.
+// N! = n*(n-1)*(n-2)* ... *2*1
+// 만약 N = 5라면 5!=5*4*3*2*1 = 120
+
+function factorial(number) {
+  const recursiveFunc = (num) => {
+    if (num === 1) {
+      return 1;
+    } else {
+      return num * recursiveFunc(num - 1);
+    }
+  };
+
+  const result = recursiveFunc(number);
+
+  return result;
+}
+
+console.log(factorial(5));
