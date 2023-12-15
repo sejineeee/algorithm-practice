e88bde64fc4415c795851a24c2e75d3d4be7fb4b
sejineeee <sejinee0918@gmail.com>
2023-12-15 17:45:34 +0900

    Add: 방향그래프(인접행렬) 알고리즘 문제 풀이

---
+++ Section9/1.js
@@ -0,0 +1,53 @@
+// 경로 탐색(인접행렬)
+// 방향그래프가 주어지면 1번 정점에서 N번 정점으로 가는 모든 경로의 가지 수를 출력하는 프로그램
+
+function getAmountOfCourse(number, list) {
+  let result = 0;
+  const resultedList = [];
+
+  const graph = Array(number + 1)
+    .fill(0)
+    .map(() => Array(number + 1).fill(0));
+  const visitedCheckList = Array(number + 1).fill(0);
+
+  for (const [a, b] of list) {
+    graph[a][b] = 1;
+  }
+  const recursiveFunc = (vertex, path) => {
+    if (vertex === number) {
+      resultedList.push([...path]);
+      return result++;
+    } else {
+      visitedCheckList[1] = 1;
+
+      for (let i = 1; i <= number; i++) {
+        if (graph[vertex][i] === 1 && visitedCheckList[i] === 0) {
+          visitedCheckList[i] = 1;
+          path.push(i);
+          recursiveFunc(i, path);
+          visitedCheckList[i] = 0;
+          path.pop();
+        }
+      }
+    }
+  };
+
+  recursiveFunc(1, [1]);
+
+  console.log(resultedList);
+  return result;
+}
+
+const N = 5;
+const inputList = [
+  [1, 2],
+  [1, 3],
+  [1, 4],
+  [2, 1],
+  [2, 3],
+  [2, 5],
+  [3, 4],
+  [4, 2],
+  [4, 5],
+];
+console.log(getAmountOfCourse(N, inputList));
