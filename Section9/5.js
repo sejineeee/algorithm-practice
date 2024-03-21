61c76fad1380b156af42a4978108eb3bec60a3b1
sejineeee <sejinee0918@gmail.com>
2024-03-21 10:44:51 +0900

    Add: BFS 알고리즘을 이용한 문제풀이

---
+++ Section9/5.js
@@ -0,0 +1,55 @@
+// 섬나라 아일랜드(BFS 활용)
+// N * N의 섬나라 아일랜드의 지도가 격자판의 정보로 주어짐
+// 각 섬은 1로 표시되어 상하좌우와 대각선으로 연결되어 있으며 0은 바다임
+
+function getCountOfIsland(gridList) {
+  const N = gridList.length;
+  const dx = [-1, -1, 0, 1, 1, 1, 0, -1];
+  const dy = [0, 1, 1, 1, 0, -1, -1, -1];
+
+  const queue = [];
+
+  let result = 0;
+
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < N; j++) {
+      if (gridList[i][j] === 1) {
+        gridList[i][j] = 0;
+        queue.push([i, j]);
+
+        while (queue.length) {
+          let [cx, cy] = queue.shift();
+          for (let k = 0; k < dx.length; k++) {
+            const nx = cx + dx[k];
+            const ny = cy + dy[k];
+            if (
+              nx >= 0 &&
+              nx < N &&
+              ny >= 0 &&
+              ny < N &&
+              gridList[nx][ny] === 1
+            ) {
+              gridList[nx][ny] = 0;
+              queue.push([nx, ny]);
+            }
+          }
+        }
+
+        result += 1;
+      }
+    }
+  }
+  return result;
+}
+
+const list = [
+  [1, 1, 0, 0, 0, 1, 0],
+  [0, 1, 1, 0, 1, 1, 0],
+  [0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 0, 1, 1],
+  [1, 1, 0, 1, 1, 0, 0],
+  [1, 0, 0, 0, 1, 0, 0],
+  [1, 0, 1, 0, 1, 0, 0],
+];
+
+console.log(getCountOfIsland(list));
